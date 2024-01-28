396913f202fd0622ef69a4428f2c93795704c972
saalid <amirrezasalari1997@gmail.com>
2024-01-28 17:16:45 +0330

    feat: complete class CancelTicketRequest

--- src/Message/CancelTicketRequest.php
+++ src/Message/CancelTicketRequest.php
@@ -2,7 +2,28 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class CancelTicketRequest
+class CancelTicketRequest extends AbstractRequest
 {
 
+    protected function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    protected function createUri(string $endpoint)
+    {
+        return $endpoint . '/payment/cancel';
+    }
+
+    protected function createResponse(array $data)
+    {
+        return new CancelTicketResponse($this, $data);
+    }
+
+    public function getData()
+    {
+        return [
+            "ticket_id" => $this->getTicketId()
+        ];
+    }
 }

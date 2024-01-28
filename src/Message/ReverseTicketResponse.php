894a9493ff2af23b1b03824101cc04d8a59191c6
saalid <amirrezasalari1997@gmail.com>
2024-01-28 17:06:11 +0330

    feat: complete ReverseTicketRespons classe

--- src/Message/ReverseTicketResponse.php
+++ src/Message/ReverseTicketResponse.php
@@ -2,7 +2,19 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class ReverseTicketResponse
+class ReverseTicketResponse extends AbstractResponse
 {
 
+    public function isSuccessful()
+    {
+        return $this->getHttpStatus() === 200 && (int)$this->getCode() === 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isCancelled()
+    {
+        return $this->getHttpStatus() === 200 && $this->getCode() !== 0;
+    }
 }

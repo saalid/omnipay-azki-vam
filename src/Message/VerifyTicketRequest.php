6738148c13cdad062693bde85e4e96ce779a1a84
Amirreza Salari <amirrzeasalari1997@gmail.com>
2024-01-28 16:33:47 +0330

    feat: complete verify ticket in request and response mode

--- src/Message/VerifyTicketRequest.php
+++ src/Message/VerifyTicketRequest.php
@@ -2,7 +2,43 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class VerifyTicketRequest
+use Omnipay\Common\Exception\InvalidRequestException;
+
+class VerifyTicketRequest extends AbstractRequest
 {
 
+    protected function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    protected function createUri(string $endpoint)
+    {
+        return $endpoint . '/payment/verify';
+    }
+
+    /**
+     * @param array $data
+     * @return VerifyTicketResponse
+     */
+    protected function createResponse(array $data)
+    {
+        return new VerifyTicketResponse($this, $data);
+    }
+
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     * @throws InvalidRequestException
+     */
+    public function getData()
+    {
+        // Validate required parameters before return data
+        
+        return [
+            'ticket_id' => $this->getTicketId()
+        ];
+    }
 }
--- src/Message/VerifyTicketResponse.php
+++ src/Message/VerifyTicketResponse.php
@@ -2,7 +2,23 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class VerifyTicketResponse
+class VerifyTicketResponse extends AbstractResponse
 {
 
+
+    /**
+     * @inheritDoc
+     */
+    public function isSuccessful()
+    {
+        return $this->getHttpStatus() === 200 && (int)$this->getCode() === 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isCancelled()
+    {
+        return $this->getHttpStatus() === 200 && $this->getCode() !== 0;
+    }
 }

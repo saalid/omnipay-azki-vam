7d5615a8d0e4e39aea1fea745eedf66d6d110482
Amirreza Salari <amirrzeasalari1997@gmail.com>
2024-01-28 14:07:14 +0330

    feat: developed methods for CreatTicket Response and Request class

--- src/Message/CreateTicketRequest.php
+++ src/Message/CreateTicketRequest.php
@@ -1,8 +1,32 @@
 <?php
 
+/**
+ * @package Omnipay\AzkiVam
+ * @author Amirreza Salari <amirrezasalari1997@gmail.com>
+ */
+
 namespace Omnipay\AzkiVam\Message;
 
-class CreateTicketRequest
+class CreateTicketRequest extends AbstractRequest
 {
 
+    protected function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    protected function createUri(string $endpoint)
+    {
+        return $endpoint . '/payment/purchase';
+    }
+
+    protected function createResponse(array $data)
+    {
+        return new CreateTicketResponse($this, $data);
+    }
+
+    public function getData()
+    {
+        // TODO: Implement getData() method.
+    }
 }
--- src/Message/CreateTicketResponse.php
+++ src/Message/CreateTicketResponse.php
@@ -1,8 +1,60 @@
 <?php
 
+/**
+ * @package Omnipay\AzkiVam
+ * @author Amirreza Salari <amirrezasalari1997@gmail.com>
+ */
+
 namespace Omnipay\AzkiVam\Message;
 
-class CreateTicketResponse
+use Omnipay\Common\Message\RedirectResponseInterface;
+
+class CreateTicketResponse extends AbstractResponse implements RedirectResponseInterface
 {
 
+    /**
+     * @return bool
+     */
+    public function isSuccessful()
+    {
+        return (int)$this->getHttpStatus() === 200 && (int)$this->getCode() === -1;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRedirect()
+    {
+        return (int)$this->getCode() === -1 &&
+            isset($this->data['trans_id']) &&
+            !empty($this->data['trans_id']);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRedirectUrl()
+    {
+        /** @var CreateTicketRequest $request */
+        $request = $this->request;
+        return sprintf('%s/payment/%s', $request->getEndpoint(), $this->getTransactionReference());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTransactionId()
+    {
+        /** @var CreateTicketRequest $request */
+        $request = $this->request;
+        return $request->getTransactionId();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTransactionReference()
+    {
+        return $this->data['trans_id'];
+    }
 }

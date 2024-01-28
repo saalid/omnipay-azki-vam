13dc546933900ecf4ddd5cf554a16ac822f9745e
saalid <amirrezasalari1997@gmail.com>
2024-01-28 16:56:32 +0330

    feat: complete methods in ReverseTicketRequest class

--- src/Message/ReverseTicketRequest.php
+++ src/Message/ReverseTicketRequest.php
@@ -2,7 +2,29 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class ReverseTicketRequest
+class ReverseTicketRequest extends AbstractRequest
 {
 
+    protected function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    protected function createUri(string $endpoint)
+    {
+        return $endpoint . '/payment/reverse';
+    }
+
+    protected function createResponse(array $data)
+    {
+        return new ReverseTicketResponse($this, $data);
+    }
+
+    public function getData()
+    {
+        return [
+            'ticket_id' => $this->getTicketId(),
+            'provider_id' => $this->getProviderId()
+        ];
+    }
 }

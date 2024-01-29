9b2e3329dbe512a3f1d0713f7349258cebd2665f
saalid <amirrezasalari1997@gmail.com>
2024-01-29 09:10:58 +0330

    feat: complete StatusTicketResponse and StatusTicketRequest

--- src/Message/StatusTicketRequest.php
+++ src/Message/StatusTicketRequest.php
@@ -2,7 +2,28 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class StatusTicketRequest
+class StatusTicketRequest extends AbstractRequest
 {
 
+    protected function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    protected function createUri(string $endpoint)
+    {
+        return $endpoint . '/payment/status';
+    }
+
+    protected function createResponse(array $data)
+    {
+        return new StatusTicketResponse($this, $data);
+    }
+
+    public function getData()
+    {
+        return [
+            'ticket_id' => $this->getTicketId()
+        ];
+    }
 }
--- src/Message/StatusTicketResponse.php
+++ src/Message/StatusTicketResponse.php
@@ -2,7 +2,22 @@
 
 namespace Omnipay\AzkiVam\Message;
 
-class StatusTicketResponse
+class StatusTicketResponse extends AbstractResponse
 {
 
+    /**
+     * @inheritDoc
+     */
+    public function isSuccessful()
+    {
+        return $this->getHttpStatus() === 200 && (int)$this->getCode() === 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isCancelled()
+    {
+        return $this->getHttpStatus() === 200 && $this->getCode() !== 0;
+    }
 }

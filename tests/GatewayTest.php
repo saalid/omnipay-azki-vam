3bd76b1c72c3b06d957cc515a6bcc548dd462159
saalid <amirrezasalari1997@gmail.com>
2024-01-29 10:17:19 +0330

    feat: write first test, setup method

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -2,7 +2,28 @@
 
 namespace Omnipay\AzkiVam\Tests;
 
-class GatewayTest
-{
+use Omnipay\AzkiVam\Gateway;
+use Omnipay\Tests\GatewayTestCase;
 
+class GatewayTest extends GatewayTestCase
+{
+    /**
+     * @var Gateway
+     */
+    protected $gateway;
+
+    /**
+     * @var array<string, integer|string|boolean>
+     */
+    protected $params;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+
+        $this->gateway->setApiKey('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+        $this->gateway->setRedirectUrl('https://www.example.com/return');
+    }
 }

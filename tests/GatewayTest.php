67ed9b079cf2e507419fc98938bb5a02d4ff2939
saalid <amirrezasalari1997@gmail.com>
2024-01-29 11:05:45 +0330

    feat: completed data for test success purchase

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -4,6 +4,7 @@ namespace Omnipay\AzkiVam\Tests;
 
 use Omnipay\AzkiVam\Gateway;
 use Omnipay\Tests\GatewayTestCase;
+use Omnipay\Omnipay;
 
 class GatewayTest extends GatewayTestCase
 {
@@ -21,9 +22,45 @@ class GatewayTest extends GatewayTestCase
     {
         parent::setUp();
 
-        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
-
+        $this->gateway = Omnipay::create('AzkiVam');
         $this->gateway->setApiKey('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
         $this->gateway->setRedirectUrl('https://www.example.com/return');
     }
+
+    public function testPurchaseSuccess(): void
+    {
+        $this->setMockHttpResponse('PurchaseSuccess.txt');
+
+
+        $paramValue= [
+            "amount" => 90,
+            "redirect_uri" => 'https://asddasd.com/asd',
+            "fallback_uri" => 'https:/test.com/failed',
+            "provider_id" => 123213213,
+            "mobile_number" => '09056619083',
+            "merchant_id" => 213213,
+            "items" => [
+                [
+                    "name" => "کالای شماره ۱",
+                    "count" => 3,
+                    "amount" => 15,
+                    "url" => "https://merchant-website/items/1",
+                ],
+                [
+                    "name" => "کالای شماره ۲",
+                    "count" => 3,
+                    "amount" => 15,
+                    "url" => "https://merchant-website/items/2",
+                ]
+            ],
+        ];
+
+        $response = $this->gateway->purchase($paramValue)->send();
+        $responseData=$response->getData();
+        self::assertTrue($response->isSuccessful());
+        self::assertTrue($response->isRedirect());
+        self::assertEquals('2c3c1fefac5a48geb9f9be7e445dd9b2',$responseData['token']);
+        self::assertEquals('https://sep.shaparak.ir/OnlinePG/SendToken?token=2c3c1fefac5a48geb9f9be7e445dd9b2', $response->getRedirectUrl());
+    }
+
 }

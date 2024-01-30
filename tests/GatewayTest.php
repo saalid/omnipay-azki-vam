3fb61e0710330b786664379cf38f120170f2bf7c
saalid <amirrezasalari1997@gmail.com>
2024-01-30 09:57:25 +0330

    feat: complete testPurchaseFailure method

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -3,6 +3,7 @@
 namespace Omnipay\AzkiVam\Tests;
 
 use Omnipay\AzkiVam\Gateway;
+use Omnipay\AzkiVam\Message\AbstractResponse;
 use Omnipay\AzkiVam\Message\CreateTicketResponse;
 use Omnipay\Tests\GatewayTestCase;
 
@@ -53,6 +54,36 @@ class GatewayTest extends GatewayTestCase
         self::assertEquals('PJQPHFwN1AM6EUAJ',$responseData['result']['ticket_id']);
         self::assertEquals('https://api.azkiloan.com/payment?ticketId=PJQPHFwN1AM6EUAJ',$responseData['result']['payment_uri']);
         self::assertEquals('https://api.azkiloan.com/payment?ticketId=PJQPHFwN1AM6EUAJ', $response->getRedirectUrl());
+    }
+
+    public function testPurchaseFailure(): void
+    {
+        $this->setMockHttpResponse('PurchaseFailure.txt');
+
+        $amount = 60;
+        $customerPhone = '09056619083';
+        $items = [
+            [
+                "name" => "کالای شماره 1",
+                "count" => 6,
+                "amount" => 10,
+                "url" => "https://merchant-website/items/1",
+            ]
+        ];
+        $subUrl = '/payment/purchase';
+
+        /** @var CreateTicketResponse $response */
+        $response = $this->gateway->purchase([
+            'subUrl' => $subUrl,
+            'amount' => $amount,
+            'customerPhone' => $customerPhone,
+            'items' => $items,
+        ])->send();
+        $responseData=$response->getData();
+        self::assertFalse($response->isSuccessful());
+        self::assertFalse($response->isRedirect());
+        self::assertEquals(18, $responseData['rsCode']);
+        self::assertEquals("Signature Invalid", $responseData['message']);
     }
 
 }
